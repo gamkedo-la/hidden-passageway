1f0b28b314752775d70cfb804910647312fbb6f1
Kornel <KORNik_k@wp.pl>
2018-05-28 11:01:56 +0200

    Added a script for a puzzle

---
+++ Assets/Scripts/Environment/MorseInteractable.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MorseInteractable : MonoBehaviour
+{
+	[SerializeField] private GameObject[] codes = null;
+	[SerializeField] private int correctCode = -1;
+
+	public bool CodeIsCorrect;// { get; private set; }
+
+	private int element = 0;
+
+	void Start ()
+	{
+		Assert.IsNotNull( codes );
+		Assert.AreNotEqual( codes.Length, 0 );
+
+		do
+		{
+			element = Random.Range( 0, codes.Length);
+
+		} while ( element == correctCode );
+		codes[element].SetActive( true );
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void triggerAction( )
+	{
+		element++;
+		element = element >= codes.Length ? 0 : element;
+
+		foreach ( GameObject code in codes )
+		{
+			code.SetActive( false );
+		}
+		codes[element].SetActive( true );
+
+		CodeIsCorrect = element == correctCode;
+	}
+}
