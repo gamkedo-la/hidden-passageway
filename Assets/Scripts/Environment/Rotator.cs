5751cc03a321a4234d4eb0de6376b32b379114b4
Trent <Xmedia101@yahoo.com>
2018-06-06 23:11:41 -0400

    More Aether terrain enhancements, upgraded float/rotate scripts
    Made the pond pretty and added mushrooms, rotators, lilypads, cattails, flowers, bushes, and upgraded the floating and water bobbing scripts.

---
+++ Assets/Scripts/Environment/Rotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public bool shouldRotate = true;
+    public bool rotateUp;
+    public bool rotateRight;
+    public float rotationSpeed = 1;
+    public Vector3 rotationVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (rotateUp) 
+        {
+            rotationVector = Vector3.up;
+        }
+        if (rotateRight) 
+        {
+            rotationVector = Vector3.right;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Rotate(rotationVector * Time.deltaTime * rotationSpeed);
+	}
+}
---
+++ Assets/Scripts/Environment/waterDriftEnhanced.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterDriftEnhanced : MonoBehaviour {
+	Vector3 startPos;
+	Quaternion startRot;
+    public float MinorVal = 0.15f;
+    public float LRcontrol = 0.34f;
+    public float UDcontrol = 0.2f;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.localPosition;
+		startRot = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = startPos + Mathf.Cos(Time.time*MinorVal)*MinorVal * Vector3.up;
+		transform.localRotation = startRot *
+			Quaternion.AngleAxis(Mathf.Cos(Time.time*LRcontrol)*1.0f,Vector3.right)*
+			Quaternion.AngleAxis(Mathf.Cos(Time.time*UDcontrol)*1.0f,Vector3.forward);
+	}
+}
