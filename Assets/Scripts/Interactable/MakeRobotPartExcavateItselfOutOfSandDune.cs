a4edb7d008853ee430e504cd420bb5a3e81a9730
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-06-08 00:34:02 -0700

    Robot Hand can now be excavated properly by holding K; still need to hook the mini power source as the trigger for excavating the Robot Hand

--- Assets/Scripts/Interactable/MakeRobotPartExcavateItselfOutOfSandDune.cs
+++ Assets/Scripts/Interactable/MakeRobotPartExcavateItselfOutOfSandDune.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class MakeRobotPartExcavateItselfOutOfSandDune : MonoBehaviour {
     public Transform robotPart;
-    public Transform posToStartTilting;
 
-    public float rateOfRotationY = 0.5f;
-    public float rateOfRotationZ = 20.0f;
+    public float rateOfRotationY = 100.0f;
+    public float rateOfRotationZ = 10.0f;
     public float rateOfRotationX = 20.0f;
 
     public float startingAngY = 0.0f;
@@ -16,15 +15,25 @@ public class MakeRobotPartExcavateItselfOutOfSandDune : MonoBehaviour {
 
     public float rateOfPositionSlide = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Excavating robot part: " + robotPart.name);
-	}
+    float stoppingPosForX = -100.0f;
+    float stoppingPosForY = 80.0f;
+    float stoppingPosForZ = -100.0f;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Excavating robot part: " + robotPart.name);    
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        RotatePart();
-        SlidePart();
+        if(Input.GetKey(KeyCode.K))
+        {
+            if ((robotPart.position.x > stoppingPosForX) && (robotPart.position.y < stoppingPosForY) && (robotPart.position.z > stoppingPosForZ))
+            {
+                RotatePart();
+                SlidePart();
+            }
+        } 
 	}
 
     void RotatePart()
