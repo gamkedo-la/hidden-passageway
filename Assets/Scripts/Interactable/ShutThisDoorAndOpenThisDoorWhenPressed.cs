e69f945c17005c9af7cdfd39821a718a6dcaddba
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-06-19 18:56:25 -0700

    thank goodness for office hours! finally, got doors to work as intended and I'm done for the night.

--- Assets/Scripts/Interactable/ShutThisDoorAndOpenThisDoorWhenPressed.cs
+++ Assets/Scripts/Interactable/ShutThisDoorAndOpenThisDoorWhenPressed.cs
@@ -4,42 +4,32 @@ using UnityEngine;
 
 public class ShutThisDoorAndOpenThisDoorWhenPressed : MonoBehaviour
 {
-    public GameObject doorToBeOpened;
-    public GameObject doorToBeClosed;
+    public doorOpenClose doorToBeOpened;
+    public doorOpenClose[] doorsInGroup;
 
-    public Transform doorToBeClosedEndPosition;
-    public Transform doorToBeOpenedEndPosition;
-
-	// Use this for initialization
-	void Start ()
-    {
-        
-    }
-	
-	// Update is called once per frame
-	void Update ()
+    //Update is called once per frame
+    void Update()
     {
         RaycastHit rayHitInfo;
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out rayHitInfo, 1.0f))
-        {
-            if(rayHitInfo.collider.tag.Contains("red button"))
-            {
-                Debug.Log("Hitting button: " + rayHitInfo.collider.name);
-                if(Input.GetMouseButtonDown(0))
-                {
-                    doorToBeOpened.transform.position = doorToBeOpenedEndPosition.position;
-                    doorToBeClosed.transform.position = doorToBeClosedEndPosition.position;
+        if (Input.GetMouseButtonDown(0))
+        {            
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out rayHitInfo, 1.0f))
+            {               
+                if (rayHitInfo.collider.gameObject == gameObject)
+                {                 
+                    OpenMyDoorOnly();
                 }
-            }
-            else if (rayHitInfo.collider.tag.Contains("blue button"))
-            {
-                Debug.Log("Hitting button: " + rayHitInfo.collider.name);
-                if (Input.GetMouseButtonDown(0))
-                {
-                    doorToBeOpened.transform.position = doorToBeOpenedEndPosition.position;
-                    doorToBeClosed.transform.position = doorToBeClosedEndPosition.position;
-                }
-            }
+            }           
         }
+    }
+
+    void OpenMyDoorOnly()
+    {
+        for(int i = 0; i < doorsInGroup.Length; i++)
+        {
+            doorsInGroup[i].Close();
+        }
+
+        doorToBeOpened.Open();
     }
 }
---
+++ Assets/doorOpenClose.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpenClose : MonoBehaviour {
+    private Transform startPos;
+    public Transform endPos;
+    public bool startsOpen;
+
+	// Use this for initialization
+	void Start () {
+        GameObject startGO = new GameObject();
+        startGO.name = gameObject.name + "_initial";
+        startGO.transform.position = transform.position;
+        startGO.transform.rotation = transform.rotation;
+        startPos = startGO.transform;
+        if(startsOpen)
+        {
+            Open();
+        }
+	}
+	
+	// Update is called once per frame
+	public void Close() {
+        transform.position = startPos.position;
+        transform.rotation = startPos.rotation;
+    }
+
+    public void Open()
+    {
+        transform.position = endPos.position;
+        transform.rotation = endPos.rotation;
+    }
+}
