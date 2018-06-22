f4b2ee2f43d94df253931acd55873d1dae2feb57
Caspar Dunant <spadxiii@gmail.com>
2018-06-22 22:34:09 +0200

    Portal script, set up portals for Platform X

---
+++ Assets/Scripts/Interactable/Portal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+	[SerializeField]
+	Transform destination;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.transform.position = destination.position;
+			other.gameObject.transform.rotation = destination.rotation;
+		}
+	}
+
+	void OnDrawGizmosSelected() {
+        if (destination != null) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, destination.position);
+        }
+    }
+}
