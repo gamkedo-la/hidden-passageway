683e563aac70c2230c793659ea0f2986e642bb81
McFunkypants <breakdance@mcfunkypants.com>
2018-06-27 14:10:19 -0700

    butterfly 2-frame animated voxel mesh and flapping controller

---
+++ Assets/ButterflyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyAI : MonoBehaviour {
+
+    public GameObject wingsOpen;
+    public GameObject wingsClosed;
+    public float flapsPerSecond = 4;
+    private int flapCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+        // do nothing if we don't have meshes to use
+        if (!wingsOpen) return;
+        if (!wingsClosed) return;
+
+        StartCoroutine("Flappy");
+    }
+
+    IEnumerator Flappy()
+    {
+        for (; ; )
+        {
+            flapCounter++;
+            if (flapCounter % 2 == 0) {
+                wingsOpen.SetActive(true);
+                wingsClosed.SetActive(false);
+            }
+            else {
+                wingsOpen.SetActive(false);
+                wingsClosed.SetActive(true);
+            }
+            yield return new WaitForSeconds(1f / flapsPerSecond);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
