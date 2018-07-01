149597bb313de4e24eb02da102a872a9603cd3cb
Chris DeLeon <gamkedo@gmail.com>
2018-07-01 11:20:39 -0700

    Adding teleport cheat keys to Aether to make it easier to demo :)

---
+++ Assets/TeleportCheat.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCheat : MonoBehaviour {
+    public GameObject playerGO;
+    public Transform[] teleList;
+
+	void Update () {
+        int whichDest = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha1) && teleList.Length>whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha2) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha3) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha4) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha5) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha6) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha7) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha8) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+            playerGO.transform.rotation = teleList[whichDest].rotation;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha9) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+        if (Input.GetKeyDown(KeyCode.Alpha0) && teleList.Length > whichDest)
+        {
+            playerGO.transform.position = teleList[whichDest].position;
+        }
+        whichDest++;
+		
+	}
+}
