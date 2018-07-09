bf1b38921ba7c3e284559b967cb8dd97c4e45fcb
McFunkypants <breakdance@mcfunkypants.com>
2018-07-08 20:06:19 -0700

    turning gears (using multiple simultaneous slideToPos)

--- Assets/Scripts/Interactable/TriggerComponentEnable.cs
+++ Assets/Scripts/Interactable/TriggerComponentEnable.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 [RequireComponent(typeof(MouseTipOnLook))]
 public class TriggerComponentEnable : MonoBehaviour {
+
+    [Header("Trigger On Click")]
     public SlideToPos toEnable;
 
+    // optional triggers fired at the SAME TIME
+    // for when you need to move two things at once
+    // for sequences, use callNext field instead
+    [Header("Optional Simultaneous Triggers")]
+    public SlideToPos toEnable2; 
+    public SlideToPos toEnable3;
+
+    [Header("Trigger Settings")]
     public bool canBeReversed = false;
 
     public void triggerAction()
@@ -13,12 +23,38 @@ public class TriggerComponentEnable : MonoBehaviour {
         if(toEnable.isDone == false)
         {
             toEnable.SendMessage("Activate");
-        } else if(canBeReversed) {
+            if (toEnable2) toEnable2.SendMessage("Activate");
+            if (toEnable3) toEnable3.SendMessage("Activate");
+
+        }
+        else if(canBeReversed) {
+
             SlideToPos endOfChain = toEnable;
             while(endOfChain.callNext != null) {
                 endOfChain = endOfChain.callNext;
             }
             endOfChain.SendMessage("Reverse");
+
+            if (toEnable2)
+            {
+                endOfChain = toEnable2;
+                while (endOfChain.callNext != null)
+                {
+                    endOfChain = endOfChain.callNext;
+                }
+                endOfChain.SendMessage("Reverse");
+            }
+
+            if (toEnable3)
+            {
+                endOfChain = toEnable3;
+                while (endOfChain.callNext != null)
+                {
+                    endOfChain = endOfChain.callNext;
+                }
+                endOfChain.SendMessage("Reverse");
+            }
+
         }
     }
 
