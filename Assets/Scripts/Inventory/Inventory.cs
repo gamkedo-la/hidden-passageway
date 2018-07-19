f055003705fb5fd6ecd5e199b8a97de01650eb4b
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-07-18 22:27:15 -0700

    Updates on Inventory/Item scripts

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -3,8 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
+    #region Singleton
+    public static Inventory instance;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory detected!");
+        }
+        instance = this;
+    }
+    #endregion
+
     public List<Item> items = new List<Item>();
     public int space = 9;
 
+    public delegate void OnItemChange();
+    public OnItemChange onItemChangeCallback;
 
+    public bool AddItem(Item item)
+    {
+        if(items.Count >= space)
+        {
+            Debug.Log("No more space in Inventory");
+            return false;
+        }
+        items.Add(item);
+
+        if(onItemChangeCallback != null)
+        {
+            onItemChangeCallback.Invoke();
+        }
+        return true;
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+        
+        if(onItemChangeCallback != null)
+        {
+            onItemChangeCallback.Invoke();
+        }
+    }
 }
--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -8,7 +8,7 @@ public class ItemPickup : MonoBehaviour {
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 1.5f))
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2.5f))
             {
                 if(item != null)
                 {
@@ -19,6 +19,13 @@ public class ItemPickup : MonoBehaviour {
     }
     void Pickup()
     {
+        bool wasItemPickedUp = Inventory.instance.AddItem(item);
 
+        if(wasItemPickedUp)
+        {
+            Debug.Log("Picking up: " + item.name);
+            Destroy(gameObject);
+        }
+        
     }
 }
