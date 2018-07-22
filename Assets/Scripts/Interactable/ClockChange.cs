9f0ea580b96507d9ea8c18f91d65aab370640c09
Chris DeLeon <gamkedo@gmail.com>
2018-07-21 21:53:38 -0500

    Clock controls added in hub, replaced placeholder book prefabs connecting to scenes

---
+++ Assets/Scripts/Interactable/ClockChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockChange : MonoBehaviour {
+    public ClockTime whichClock;
+    public float minChange = 15.0f;
+
+	private void Start()
+	{
+        if (whichClock == null)
+        {
+            Debug.LogError("Warning: missing clock for " + gameObject.name);
+        }
+	}
+	public void triggerAction() {
+        whichClock.targetTime += minChange;
+        // keeps it on even increments of 5 min.
+        whichClock.targetTime = Mathf.Round(whichClock.targetTime / 5.0f) * 5.0f;
+	}
+}
--- Assets/Scripts/Interactable/ClockTime.cs
+++ Assets/Scripts/Interactable/ClockTime.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class ClockTime : MonoBehaviour {
     public Transform hourHand;
     public Transform minHand;
+    public float targetTime = 0.0f;
     private float minutesNow=0.0f;
 	// Use this for initialization
 	void Start () {
-		
+        minutesNow = targetTime;
 	}
-	
+
+	private void FixedUpdate()
+	{
+        float kVal = 0.9f;
+        minutesNow = targetTime * (1.0f-kVal) + minutesNow * kVal;
+	}
+
 	// Update is called once per frame
 	void Update () {
-        minutesNow += Time.deltaTime * 20.0f;
         // Debug.Log(minutesNow);
         float offsetForZeroDegAtTop = -90.0f;
         minHand.rotation = Quaternion.AngleAxis(minutesNow * 360.0f / 60.0f
