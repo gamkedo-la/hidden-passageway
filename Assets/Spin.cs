c0c1407405ded87cc1dd4312d91bc3620bcc1503
Chris DeLeon <gamkedo@gmail.com>
2018-08-03 16:11:21 -0700

    Added spinning globe to hub scratch area

---
+++ Assets/Spin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime * -30.0f);		
+	}
+}
