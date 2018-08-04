9c72a62f7619aa2e10ca42aa8c89999a865eab3d
Chris DeLeon <gamkedo@gmail.com>
2018-08-04 12:25:52 -0700

    First pass on lat and long controller for globe

---
+++ Assets/LatLongGlobe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatLongGlobe : MonoBehaviour {
+    public float targetLat = 0.0f;
+    public float targetLong = 0.0f;
+    private float latNow = 0.0f;
+    private float longNow = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        latNow = targetLat;
+        longNow = targetLong;
+    }
+
+    private void FixedUpdate()
+    {
+        float kVal = 0.9f;
+        latNow = targetLat * (1.0f - kVal) + targetLat * kVal;
+        longNow = targetLong * (1.0f - kVal) + targetLong * kVal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(minutesNow);
+        // float offsetForZeroDegAtTop = -90.0f;
+        transform.rotation = Quaternion.AngleAxis(latNow
+                                                  /*+ offsetForZeroDegAtTop*/, Vector3.right)
+            * Quaternion.AngleAxis(longNow
+                                   /*+ offsetForZeroDegAtTop*/, Vector3.up);
+    }
+}
---
+++ Assets/Scripts/Interactable/LatLongGlobeChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatLongGlobeChange : MonoBehaviour {
+    public LatLongGlobe whichGlobe;
+    public float minChange = 15.0f;
+    public bool isForLatNotLong = false;
+
+	private void Start()
+	{
+        if (whichGlobe == null)
+        {
+            Debug.LogError("Warning: missing clock for " + gameObject.name);
+        }
+	}
+	public void triggerAction() {
+        if (isForLatNotLong)
+        {
+            whichGlobe.targetLat += minChange;
+            whichGlobe.targetLat = Mathf.Round(whichGlobe.targetLat / 5.0f) * 5.0f;
+        }
+        else
+        {
+            whichGlobe.targetLong += minChange;
+            whichGlobe.targetLong = Mathf.Round(whichGlobe.targetLong / 5.0f) * 5.0f;
+        }
+	}
+}
