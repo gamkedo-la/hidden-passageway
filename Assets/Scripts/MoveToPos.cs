20296009fd70039a469d00498cec2e4541936cf4
Jaime Rivas <jaimerivas81@outlook.com>
2018-08-26 21:07:25 -0700

    Duplicated scene while I work on Nav Agent; Follows player (trying to figure out how to make it follow the player when they jump onto the huge voxels; might just adjust the scale of all the GOs)

---
+++ Assets/Scripts/MoveToPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class MoveToPos : MonoBehaviour {
+    private NavMeshAgent agent;
+    [SerializeField] private Transform goal;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Assert.IsNotNull(agent, "YO! There's no NavMeshAgent!");
+	}
+	
+	void Update () {
+        agent.destination = goal.position;
+        agent.transform.LookAt(goal, Vector3.up);
+	}
+}
