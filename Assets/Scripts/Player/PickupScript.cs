a99e7affbdc3853efe16c0939c46cb6738893d3b
Caspar Dunant <spadxiii@gmail.com>
2018-09-13 20:54:11 +0200

    In Platform X the ball can be picked up now

--- Assets/Scripts/Player/PickupScript.cs
+++ Assets/Scripts/Player/PickupScript.cs
@@ -8,20 +8,17 @@ public class PickupScript : MonoBehaviour {
     /// gamepad controls for player and this script
     /// generalizing script further to pass on to the other level designers
     /// sleep ;-;
-    /// 
-    /// 
+    ///
+    ///
     /// </Todo>
 
-
-    [SerializeField]
-    private float ThrowForce = 10;
-    public bool canHold = true;
-    public bool hideCol;
-    public GameObject item;
-    public Collider itemCol;
-    public Transform guide;
-    public float rotationForce;
-    public GameObject exception;
+    [SerializeField] private float ThrowForce = 10;
+    [SerializeField] private bool canHold = true;
+    [SerializeField] private Transform guide;
+    [SerializeField] private float rotationForce;
+    private GameObject item;
+    private Collider itemCollider;
+    private GameObject exception;
 
     private void Start()
     {
@@ -32,34 +29,48 @@ public class PickupScript : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Click");
+            // Debug.Log("Click");
             if (!canHold)
-                throw_drop();
+            {
+                ThrowDrop();
+            }
             else
+            {
                 Pickup();
+            }
         }//mause If
 
         if (!canHold && item)
+        {
             item.transform.position = guide.position;
+        }
 
     }//update
 
     //We can use trigger or Collision
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "item"|| col.gameObject.tag == "keyItem")
-            //Debug.Log("Collision Detected With Item");
-            if (!item) // if we don't have anything holding
+        if (col.gameObject.tag == Tags.Item || col.gameObject.tag == Tags.KeyItem)
+        {
+            // Debug.Log("Collision Detected With Item");
+            if (!item)
+            {
+                // if we don't have anything holding
                 item = col.gameObject;
+            }
+        }
     }
 
     //We can use trigger or Collision
     void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.tag == "item" || col.gameObject.tag == "keyItem")
+        if (col.gameObject.tag == Tags.Item || col.gameObject.tag == Tags.KeyItem)
         {
+            // Debug.Log("TriggerExit Detected With Item");
             if (canHold)
+            {
                 item = null;
+            }
         }
     }
 
@@ -71,14 +82,18 @@ public class PickupScript : MonoBehaviour {
     private void Pickup()
     {
         if (!item)
+        {
+            // Debug.Log("No item to pick up");
             return;
+        }
 
-
-        //parent an item to the guide
+        // Parent an item to the guide
         item.transform.SetParent(guide);
 
-        //turn off gravity
-        item.GetComponent<Rigidbody>().useGravity = false;
+        // Turn off gravity and rotation
+        Rigidbody itemRb = item.GetComponent<Rigidbody>();
+        itemRb.useGravity = false;
+        itemRb.freezeRotation = true;
 
         //add cam rotation to keep it straight
         item.transform.localRotation = transform.rotation;
@@ -86,39 +101,44 @@ public class PickupScript : MonoBehaviour {
         item.transform.position = guide.position;
 
         canHold = false;
-        itemCol = item.GetComponent<Collider>();
+        itemCollider = item.GetComponent<Collider>();
         if (item == exception)
         {
-            itemCol.enabled = true;
+            itemCollider.enabled = true;
         }
         else
         {
-            itemCol.enabled = false;
+            itemCollider.enabled = false;
         }
     }
 
-    // throwing or dropping method
-    private void throw_drop()
+    private void ThrowDrop()
     {
-
         if (!item)
-            return;
-        if (!(item == exception))
         {
-            itemCol.enabled = true;
+            return;
+        }
+        if (item != exception)
+        {
+            itemCollider.enabled = true;
         }
 
-        //Set our Gravity to true again.
-        item.GetComponent<Rigidbody>().useGravity = true;
+        // Enable gravity and rotation
+        Rigidbody itemRb = item.GetComponent<Rigidbody>();
+        itemRb.useGravity = true;
+        itemRb.freezeRotation = false;
+
         // blank out item field to open up space for the next
         item = null;
+
         //Apply force on throwing
-        guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * ThrowForce;
+        if (ThrowForce != 0.0f) {
+            guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * ThrowForce;
+        }
 
         //Unparent item
         guide.GetChild(0).parent = null;
         canHold = true;
-        itemCol.enabled = true;
-
+        itemCollider.enabled = true;
     }
 }
