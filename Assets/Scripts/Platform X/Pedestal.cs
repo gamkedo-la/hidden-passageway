b1133d18382859d681b9b808aeb7421ce14373e3
Caspar Dunant <spadxiii@gmail.com>
2018-09-18 21:37:44 +0200

    Reset ball position when dropped off the platform, trigger pedestal when a ball is dropped on it (temp effect for testing)

---
+++ Assets/Scripts/Platform X/Pedestal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestal : MonoBehaviour {
+
+	private bool isActive = false;
+
+	void OnTriggerStay(Collider other)
+	{
+		PuzzleBall pb = other.GetComponent<PuzzleBall>();
+		if (pb != null) {
+			isActive = true;
+		}
+	}
+
+	void Update () {
+		if (isActive) {
+			transform.Rotate(Vector3.up, 10 * Time.deltaTime);
+		}
+
+		// Will be re-enabled in OnTriggerStay if there's still a ball
+		isActive = false;
+	}
+}
---
+++ Assets/Scripts/Platform X/PuzzleBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleBall : MonoBehaviour {
+
+	[SerializeField] private float resetPositionY = -30f;
+
+	private Rigidbody rb;
+	private Vector3 resetPosition;
+	private Quaternion resetRotation;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+		resetPosition = transform.position;
+		resetRotation = transform.rotation;
+	}
+
+	void Update () {
+		if (transform.position.y <= resetPositionY) {
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+			transform.position = resetPosition;
+			transform.rotation = resetRotation;
+		}
+	}
+}
