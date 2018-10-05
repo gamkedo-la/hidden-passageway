b524f89ae30a6be57ad404a630ad1434d5905969
sjhoffiz <sj.hoffiz@gmail.com>
2018-10-04 22:59:17 -0400

    Battery Cab be Carried and release on contact with received

---
+++ Assets/PickUpCell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCell : MonoBehaviour {
+    public Transform carrySpot;
+    public bool isCarrying = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (isCarrying) {
+            transform.position = carrySpot.position;
+            transform.rotation = carrySpot.rotation;  
+        }
+
+	}
+}
---
+++ Assets/ReceivedBattery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceivedBattery : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PickUpCell puc = other.GetComponent<PickUpCell>();
+        if (puc){
+            puc.enabled = false;
+            Debug.Log("battery received");  
+        }
+
+    }
+}
