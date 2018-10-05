35f3b0bd36b36832c1e5e3ba0de0717c76d8eb18
sjhoffiz <sj.hoffiz@gmail.com>
2018-10-04 22:22:48 -0400

    Light Beam Turns off when player close & teleport cheats

---
+++ Assets/TurnOutTheLights.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOutTheLights : MonoBehaviour {
+    public GameObject lightsGO;
+	private void OnTriggerEnter(Collider other)
+	{
+        Debug.Log("enter the trigger");
+        lightsGO.SetActive(false);
+    }
+
+}
