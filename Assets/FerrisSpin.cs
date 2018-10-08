033c2b9ce998f34ade27e62faa77dd754d50e15f
Chris DeLeon <gamkedo@gmail.com>
2018-10-08 16:19:32 -0700

    Theme park added by city in distrance to use dviruet's ferris wheel

---
+++ Assets/FerrisSpin.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FerrisSpin : MonoBehaviour {
+	void Update () {
+        transform.Rotate(Vector3.right, 10.0f * Time.deltaTime);
+	}
+}
---
+++ Assets/StayUpright.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayUpright : MonoBehaviour {
+    Quaternion startRot;
+	// Use this for initialization
+	void Start () {
+        startRot = transform.rotation;
+	}
+	
+	void LateUpdate () {
+        transform.rotation = startRot;
+	}
+}
