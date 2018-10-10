51ce167efc4d93502caac6298216bc735185cad6
Chris DeLeon <gamkedo@gmail.com>
2018-10-09 18:25:46 -0700

    Got the cat to peek up out of laundry hamper if looked at

---
+++ Assets/MoveIfLookedAt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIfLookedAt : MonoBehaviour {
+    public Transform moveThis;
+    public Transform fromHere;
+    public Transform toHere;	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit rhInfo;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out rhInfo, 4.0f))
+        {
+            Transform whichTo;
+            if (rhInfo.collider == gameObject.GetComponent<Collider>())
+            {
+                whichTo = toHere;
+            }
+            else
+            {
+                whichTo = fromHere;
+            }
+            moveThis.position = Vector3.Lerp(moveThis.position, whichTo.position, 0.1f);
+            moveThis.rotation = Quaternion.Slerp(moveThis.rotation, whichTo.rotation, 0.1f);
+        }
+	}
+}
