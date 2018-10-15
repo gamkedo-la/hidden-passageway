1d304a11b3699a938ee44d080bdeab81e18d44d7
Chris DeLeon <gamkedo@gmail.com>
2018-10-14 19:11:54 -0700

    Updated scrap papers, now headed out to dinner :)

--- Assets/Scripts/General/EscToQuitForBuilds.cs
+++ Assets/Scripts/General/EscToQuitForBuilds.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EscToQuitForBuilds : MonoBehaviour {
 
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape)) {
+		/*if(Input.GetKeyDown(KeyCode.Escape)) {
             Debug.Log("EscToQuitForBuilds script is no longer used");
 			// Application.Quit();
-		}
+		}*/
 	}
 }
