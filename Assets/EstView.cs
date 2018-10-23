19d06e8946630f4db32371b92d7cdcc7291ae6f6
Chris DeLeon <gamkedo@gmail.com>
2018-10-23 15:06:54 -0700

    Experimenting with establishing view (so far only used in Main Hub)

---
+++ Assets/EstView.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EstView : MonoBehaviour {
+    public Transform[] viewWaypoints;
+    public float duration = 5.0f;
+    public Text progressUpdate;
+    private int wpNow = 0;
+    private float durationStart = 0.0f;
+    private Camera useCamera;
+    private Transform wasCamParent;
+    private GameObject playerGO;
+
+    private Vector3 wasPos;
+    private Quaternion wasRot;
+
+    GameObject menuGO, aimerGO, mouseLookGO;
+    GameObject TipTextShadowGO, TipTextFGGO;
+
+	// Use this for initialization
+	void Start () {
+        if (SceneWarp.fromScene != null && SceneWarp.fromScene != "MainHub")
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        menuGO = GameObject.Find("MenuHub");
+        aimerGO = GameObject.Find("Aimer");
+        mouseLookGO = GameObject.Find("MouseLockTip");
+        TipTextShadowGO = GameObject.Find("TipTextShadow");
+        TipTextFGGO = GameObject.Find("TipTextFG");
+
+        TipTextShadowGO.GetComponent<Text>().text = "";;
+        TipTextFGGO.GetComponent<Text>().text = "";;
+        menuGO.SetActive(false);
+        aimerGO.SetActive(false);
+        if (mouseLookGO)
+        {
+            mouseLookGO.SetActive(false);
+        }
+
+        GameObject camGO = GameObject.Find("TransitionCam");
+        useCamera = camGO.GetComponent<Camera>();
+        wasCamParent = useCamera.transform.parent;
+        useCamera.transform.SetParent(null);
+        wasPos = useCamera.transform.position;
+        wasRot = useCamera.transform.rotation;
+        playerGO = GameObject.Find("Player");
+        playerGO.SetActive(false);
+	}
+
+    void QuitEst()
+    {
+        useCamera.transform.position = wasPos;
+        useCamera.transform.rotation = wasRot;
+        playerGO.SetActive(true);
+        menuGO.SetActive(true);
+        aimerGO.SetActive(true);
+        if (mouseLookGO)
+        {
+            mouseLookGO.SetActive(true);
+        }
+        useCamera.transform.SetParent(wasCamParent);
+        gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1") ||
+           Input.GetMouseButtonDown(0))
+        {
+            QuitEst();
+            return;
+        }
+
+        float lerpPt = (Time.timeSinceLevelLoad-durationStart) / duration;
+        if (lerpPt > 1.0f)
+        {
+            wpNow+=2;
+            if (wpNow + 1 >= viewWaypoints.Length)
+            {
+                QuitEst();
+                return;
+            }
+            else
+            {
+                durationStart = Time.timeSinceLevelLoad;
+                lerpPt = 0.0f;
+            }
+        }
+
+        int wpPairs = Mathf.FloorToInt(viewWaypoints.Length * 0.5f);
+        int pairYet = Mathf.FloorToInt(wpNow * 0.5f);
+        float percPerSegment = 1.0f / wpPairs;
+        float percentage = pairYet * percPerSegment + lerpPt * percPerSegment;
+        float totalTime = duration * wpPairs;
+        string percSoFar = ""+Mathf.FloorToInt( (1.0f-percentage) * totalTime);
+        if (percSoFar.Length < 2)
+        {
+            percSoFar = "0" + percSoFar;
+        }
+        progressUpdate.text = "Click to Skip Intro (0:"+percSoFar+")";
+
+        useCamera.transform.position = Vector3.Lerp(viewWaypoints[wpNow].position,viewWaypoints[wpNow+1].position, lerpPt);
+        useCamera.transform.rotation = Quaternion.Slerp(viewWaypoints[wpNow].rotation, viewWaypoints[wpNow + 1].rotation, lerpPt);
+	}
+}
