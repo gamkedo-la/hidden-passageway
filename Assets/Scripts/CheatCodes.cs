86c71002feeb8a1cbf3238d9b3b5bba75b07e3ce
KornelGM <KORNik_k@wp.pl>
2018-10-29 18:58:13 +0100

    Changed "mod" keys for cheat codes and made turning off music more reliable, as per Chris' suggestion

--- Assets/Scripts/CheatCodes.cs
+++ Assets/Scripts/CheatCodes.cs
@@ -3,29 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class CheatCodes : MonoBehaviour
 {
-	void Update ()
+	void LateUpdate ()
 	{
 		// Check Cheat Code for Levels 0-9
-		CheckCheatCode( KeyCode.Alpha0, 0, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha1, 1, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha2, 2, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha3, 3, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha4, 4, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha5, 5, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha6, 6, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha7, 7, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha8, 8, KeyCode.End );
-		CheckCheatCode( KeyCode.Alpha9, 9, KeyCode.End );
+		CheckCheatCode( KeyCode.Alpha0, 0, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha1, 1, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha2, 2, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha3, 3, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha4, 4, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha5, 5, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha6, 6, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha7, 7, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha8, 8, KeyCode.LeftBracket );
+		CheckCheatCode( KeyCode.Alpha9, 9, KeyCode.LeftBracket );
 
 		// Check Cheat Code for Levels 10-17
-		CheckCheatCode( KeyCode.Alpha0, 10, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha1, 11, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha2, 12, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha3, 13, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha4, 14, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha5, 15, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha6, 16, KeyCode.Home );
-		CheckCheatCode( KeyCode.Alpha7, 17, KeyCode.Home );
+		CheckCheatCode( KeyCode.Alpha0, 10, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha1, 11, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha2, 12, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha3, 13, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha4, 14, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha5, 15, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha6, 16, KeyCode.RightBracket );
+		CheckCheatCode( KeyCode.Alpha7, 17, KeyCode.RightBracket );
 	}
 
 	private void CheckCheatCode(KeyCode mainKey, int level, KeyCode modKey )
